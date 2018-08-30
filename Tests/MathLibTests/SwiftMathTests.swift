1acccedb5471756b4d33f7fba93792674721f082
Pavel Rytir <pavelrt@icloud.com>
2018-08-30 15:46:55 +0200

    Tests fixed.

--- Tests/MathLibTests/SwiftMathTests.swift
+++ Tests/MathLibTests/SwiftMathTests.swift
@@ -9,37 +9,37 @@ final class MathLibTests: XCTestCase {
         
         let distanceToEnd = distances[end.id]!
         XCTAssertEqual(distanceToEnd, 1000.0)
-        XCTAssertEqual(paths[end.id]!.count, 1001) // Number of vertices on the path
+        XCTAssertEqual(paths[end.id]!.count, 1000) // Number of vertices on the path
     }
 
     
     func testDijkstraTestCircuit() {
         let circuit = createDiCircuit(length: 1000)
-        let v1 = circuit.vertices.first!
-        let inEdge = circuit.edges[v1.value.inEdges.first!]
+        let v1 = circuit.diVertices.first!
+        let inEdge = circuit.diEdges[v1.value.inEdges.first!]
         let v2Id = inEdge!.start
     
         let (distances, paths) = shortestPathsDijkstra(in: circuit, sourceId: v1.key, pathTo: [v2Id], lengths: { _ in return 1.0 })
         
         let distanceToEnd = distances[v2Id]!
         XCTAssertEqual(distanceToEnd, 999.0)
-        XCTAssertEqual(paths[v2Id]!.count, 1000) // Number of vertices on the path
+        XCTAssertEqual(paths[v2Id]!.count, 999) // Number of vertices on the path
     }
     
     func testCreateGrid1() {
         let (grid, _) = createDiGrid(dimensions: 10)
-        XCTAssertEqual(grid.vertices.count, 10)
-        XCTAssertEqual(grid.edges.count, 18)
+        XCTAssertEqual(grid.verticesCount, 10)
+        XCTAssertEqual(grid.diEdges.count, 18)
     }
     func testCreateGrid2() {
         let (grid, _) = createDiGrid(dimensions: 10, 10)
-        XCTAssertEqual(grid.vertices.count, 100)
-        XCTAssertEqual(grid.edges.count, 18 * 10 + 18 * 10)
+        XCTAssertEqual(grid.diVertices.count, 100)
+        XCTAssertEqual(grid.diEdges.count, 18 * 10 + 18 * 10)
     }
     func testCreateGrid3() {
         let (grid, _) = createDiGrid(dimensions: 10, 10, 10)
-        XCTAssertEqual(grid.vertices.count, 1000)
-        XCTAssertEqual(grid.edges.count, ((18 * 10 + 18 * 10) * 10) + (10 * 18 * 10))
+        XCTAssertEqual(grid.diVertices.count, 1000)
+        XCTAssertEqual(grid.diEdges.count, ((18 * 10 + 18 * 10) * 10) + (10 * 18 * 10))
     }
     func testDijkstraGrid3() {
         let (grid, indexer) = createDiGrid(dimensions: 10, 10, 10)
@@ -48,7 +48,7 @@ final class MathLibTests: XCTestCase {
         let (distances, paths) = shortestPathsDijkstra(in: grid, sourceId: start, pathTo: [end], lengths: { _ in return 1.0 })
         let distanceToEnd = distances[end]!
         XCTAssertEqual(distanceToEnd, 3 * 9.0)
-        XCTAssertEqual(paths[end]!.count, 3 * 9 + 1) // Number of vertices on the path
+        XCTAssertEqual(paths[end]!.count, 3 * 9) // Number of vertices on the path
     }
     
     
