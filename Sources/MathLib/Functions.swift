efa4e64ea35ea4fd68f42e3afd3e45c6f0de9912
Pavel Rytir <pavelrt@icloud.com>
2019-11-18 21:35:29 +0100

    Added GCD algorithm

--- Sources/MathLib/Functions.swift
+++ Sources/MathLib/Functions.swift
@@ -11,3 +11,9 @@ import Foundation
 public func sigmoid(_ x: Double) -> Double {
     return 1/(1 + exp(-x))
 }
+
+public func gcd(a: Int, b:Int) -> Int {
+    precondition(a>=0)
+    precondition(b>=0)
+    return b == 0 ? a : gcd(a: b, b: a % b)
+}
